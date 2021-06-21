fc0cc76347d608244433621a9d6a5e4d0d7be41f
DESKTOP-510\userpc <tom881205@naver.com>
2021-06-21 13:28:05 +0900

    수강신청 js 수정

--- jnang-web/src/main/webapp/resource/js/lecture/lecture.list.js
+++ jnang-web/src/main/webapp/resource/js/lecture/lecture.list.js
@@ -200,7 +200,7 @@ var searchLectureList = function(){
 						
                     		if(data.resultList[i].webCapa-data.resultList[i].webUser === 0 || data.resultList[i].webCapa-data.resultList[i].webUser < 0){
                     				//준비중 팝업창 6월 24일 아침 7시에 구민센터 해당코드 삭제
-                    				/*if(data.resultList[i].comcd == 'JUNGNANG01'){
+                    				if(data.resultList[i].comcd == 'JUNGNANG01'){
                     					tableBody += '<td><a class="size_s2 btn_pink_redWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-3423-1070");\'>준비중</a></td>';
                     				}else if(data.resultList[i].comcd == 'JUNGNANG02'){
                     					tableBody += '<td><a class="size_s2 btn_pink_redWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-436-9200");\'>준비중</a></td>';
@@ -214,11 +214,11 @@ var searchLectureList = function(){
                     					tableBody += '<td><a class="size_s2 btn_pink_redWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다.");\'>준비중</a></td>';
                     				}else{
                     					tableBody += '<td><a class="size_s2 btn_pink_redWrite">준비중</a></td>';
-                    				}*/
-                    				tableBody += '<td><a class="size_s2 btn_pink_redWrite">준비중</a></td>';
+                    				}
+                    				//tableBody += '<td><a class="size_s2 btn_pink_redWrite">준비중</a></td>';
 							}else{
 									//6월 24일 아침 7시에 구민체육센터 해당코드 삭제해야함!!!! 그때부터 접수가 가능하도록								
-									/*if(data.resultList[i].comcd == 'JUNGNANG01'){
+									if(data.resultList[i].comcd == 'JUNGNANG01'){
 										tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-3423-1070");\'>준비중</a></td>';
 									}else if(data.resultList[i].comcd == 'JUNGNANG02'){
 										tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-436-9200");\'>준비중</a></td>';
@@ -232,12 +232,12 @@ var searchLectureList = function(){
 										tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다.");\'>준비중</a></td>';
 									}else{
 										tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick="lecturePaymentDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[0].itemCd+'\'\,'+data.resultList[i].programItem[0].sold+','+data.resultList[i].programItem[0].eold+','+data.resultList[i].grpcd.recSdate+','+data.resultList[i].grpcd.recEdate+');">수강신청</a></td>';
-									}*/
-									tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick="lecturePaymentDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[0].itemCd+'\'\,'+data.resultList[i].programItem[0].sold+','+data.resultList[i].programItem[0].eold+','+data.resultList[i].grpcd.recSdate+','+data.resultList[i].grpcd.recEdate+');">수강신청</a></td>';
+									}
+									//tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick="lecturePaymentDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[0].itemCd+'\'\,'+data.resultList[i].programItem[0].sold+','+data.resultList[i].programItem[0].eold+','+data.resultList[i].grpcd.recSdate+','+data.resultList[i].grpcd.recEdate+');">수강신청</a></td>';
 									
 							}
                     	//6월 24일 아침 7시에 구민센터 상세보기 가능하도록 해야함 
-						/*if(data.resultList[i].comcd == 'JUNGNANG01'){
+						if(data.resultList[i].comcd == 'JUNGNANG01'){
         					tableBody += '<td><a class="size_s2 btn_blue2" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-3423-1070");\'>준비중</a></td>';
         				}else if(data.resultList[i].comcd == 'JUNGNANG02'){
         					tableBody += '<td><a class="size_s2 btn_blue2" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-436-9200");\'>준비중</a></td>';
@@ -251,8 +251,8 @@ var searchLectureList = function(){
         					tableBody += '<td><a class="size_s2 btn_blue2" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다.");\'>준비중</a></td>';
         				}else{
         					tableBody += '<td ><a class="size_s2 btn_blue2" onclick="searchLectureDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].sportsCd+'\'\, \''+data.resultList[i].msportsCd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[0].itemCd+'\');">상세보기</a></td>';
-        				}*/								
-						tableBody += '<td ><a class="size_s2 btn_blue2" onclick="searchLectureDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].sportsCd+'\'\, \''+data.resultList[i].msportsCd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[0].itemCd+'\');">상세보기</a></td>';
+        				}								
+						//tableBody += '<td ><a class="size_s2 btn_blue2" onclick="searchLectureDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].sportsCd+'\'\, \''+data.resultList[i].msportsCd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[0].itemCd+'\');">상세보기</a></td>';
 						tableBody += '</tr>';
 				
 						
@@ -283,7 +283,7 @@ var searchLectureList = function(){
 								
 							if(data.resultList[i].webCapa-data.resultList[i].webUser === 0 || data.resultList[i].webCapa-data.resultList[i].webUser < 0){
 									//준비중 팝업창 6월 24일 아침 7시에 구민센터 해당코드 삭제
-									/*if(data.resultList[i].comcd == 'JUNGNANG01'){
+									if(data.resultList[i].comcd == 'JUNGNANG01'){
 	                					tableBody += '<td><a class="size_s2 btn_pink_redWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-3423-1070");\'>준비중</a></td>';
 	                				}else if(data.resultList[i].comcd == 'JUNGNANG02'){
 	                					tableBody += '<td><a class="size_s2 btn_pink_redWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-436-9200");\'>준비중</a></td>';
@@ -297,11 +297,11 @@ var searchLectureList = function(){
 	                					tableBody += '<td><a class="size_s2 btn_pink_redWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다.");\'>준비중</a></td>';
 	                				}else{
 	                					tableBody += '<td><a class="size_s2 btn_pink_redWrite">준비중</a></td>';
-	                				}*/
-									tableBody += '<td><a class="size_s2 btn_pink_redWrite">준비중</a></td>';
+	                				}
+									//tableBody += '<td><a class="size_s2 btn_pink_redWrite">준비중</a></td>';
 							}else{
 								//6월 24일 아침 7시에 구민체육센터 해당코드 삭제해야함!!!! 그때부터 접수가 가능하도록
-								/*if(data.resultList[i].comcd == 'JUNGNANG01'){
+								if(data.resultList[i].comcd == 'JUNGNANG01'){
 									tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-3423-1070");\'>준비중</a></td>';
 								}else if(data.resultList[i].comcd == 'JUNGNANG02'){
 									tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-436-9200");\'>준비중</a></td>';
@@ -315,13 +315,13 @@ var searchLectureList = function(){
 									tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다.");\'>준비중</a></td>';
 								}else{
 									tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick="lecturePaymentDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[0].itemCd+'\'\,'+data.resultList[i].programItem[0].sold+','+data.resultList[i].programItem[0].eold+','+data.resultList[i].grpcd.recSdate+','+data.resultList[i].grpcd.recEdate+');">수강신청</a></td>';
-								}*/
-								tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick="lecturePaymentDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[j].itemCd+'\'\,'+data.resultList[i].programItem[0].sold+','+data.resultList[i].programItem[0].eold+','+data.resultList[i].grpcd.recSdate+','+data.resultList[i].grpcd.recEdate+');">수강신청</a></td>';
+								}
+								//tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick="lecturePaymentDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[j].itemCd+'\'\,'+data.resultList[i].programItem[0].sold+','+data.resultList[i].programItem[0].eold+','+data.resultList[i].grpcd.recSdate+','+data.resultList[i].grpcd.recEdate+');">수강신청</a></td>';
 																
 							}
 							
 							//6월 24일 아침 7시에 구민센터 상세보기 가능하도록 해야함
-							/*if(data.resultList[i].comcd == 'JUNGNANG01'){
+							if(data.resultList[i].comcd == 'JUNGNANG01'){
 	        					tableBody += '<td><a class="size_s2 btn_blue2" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-3423-1070");\'>준비중</a></td>';
 	        				}else if(data.resultList[i].comcd == 'JUNGNANG02'){
 	        					tableBody += '<td><a class="size_s2 btn_blue2" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-436-9200");\'>준비중</a></td>';
@@ -335,8 +335,8 @@ var searchLectureList = function(){
 	        					tableBody += '<td><a class="size_s2 btn_blue2" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다.");\'>준비중</a></td>';
 	        				}else{
 	        					tableBody += '<td ><a class="size_s2 btn_blue2" onclick="searchLectureDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].sportsCd+'\'\, \''+data.resultList[i].msportsCd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[0].itemCd+'\');">상세보기</a></td>';
-	        				}*/
-							tableBody += '<td ><a class="size_s2 btn_blue2" onclick="searchLectureDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].sportsCd+'\'\, \''+data.resultList[i].msportsCd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[j].itemCd+'\');">상세보기</a></td>';
+	        				}
+								//tableBody += '<td ><a class="size_s2 btn_blue2" onclick="searchLectureDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].sportsCd+'\'\, \''+data.resultList[i].msportsCd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[j].itemCd+'\');">상세보기</a></td>';
 								tableBody += '</tr>';
 							}else{
 								tableBody += '<tr>';   
@@ -347,7 +347,7 @@ var searchLectureList = function(){
 								if(data.resultList[i].webCapa-data.resultList[i].webUser === 0 || data.resultList[i].webCapa-data.resultList[i].webUser < 0){
 									
 									//준비중 팝업창 6월 24일 아침 7시에 구민센터 해당코드 삭제
-									/*if(data.resultList[i].comcd == 'JUNGNANG01'){
+									if(data.resultList[i].comcd == 'JUNGNANG01'){
 										tableBody += '<td><a class="size_s2 btn_pink_redWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-3423-1070");\'>준비중</a></td>';
 									}else if(data.resultList[i].comcd == 'JUNGNANG02'){
 										tableBody += '<td><a class="size_s2 btn_pink_redWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-436-9200");\'>준비중</a></td>';
@@ -361,11 +361,11 @@ var searchLectureList = function(){
 										tableBody += '<td><a class="size_s2 btn_pink_redWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다.");\'>준비중</a></td>';
 									}else{
 										tableBody += '<td><a class="size_s2 btn_pink_redWrite">준비중</a></td>';
-									}*/
-									tableBody += '<td><a class="size_s2 btn_pink_redWrite">준비중</a></td>';
+									}
+									//tableBody += '<td><a class="size_s2 btn_pink_redWrite">준비중</a></td>';
 								}else{
 									//6월 24일 아침 7시에 구민체육센터 해당코드 삭제해야함!!!! 그때부터 접수가 가능하도록
-									/*if(data.resultList[i].comcd == 'JUNGNANG01'){
+									if(data.resultList[i].comcd == 'JUNGNANG01'){
 										tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-3423-1070");\'>준비중</a></td>';
 									}else if(data.resultList[i].comcd == 'JUNGNANG02'){
 										tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-436-9200");\'>준비중</a></td>';
@@ -379,13 +379,13 @@ var searchLectureList = function(){
 										tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다.");\'>준비중</a></td>';
 									}else{
 										tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick="lecturePaymentDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[0].itemCd+'\'\,'+data.resultList[i].programItem[0].sold+','+data.resultList[i].programItem[0].eold+','+data.resultList[i].grpcd.recSdate+','+data.resultList[i].grpcd.recEdate+');">수강신청</a></td>';
-									}*/
-									tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick="lecturePaymentDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[j].itemCd+'\'\,'+data.resultList[i].programItem[0].sold+','+data.resultList[i].programItem[0].eold+','+data.resultList[i].grpcd.recSdate+','+data.resultList[i].grpcd.recEdate+');">수강신청</a></td>';
+									}
+									//tableBody += '<td ><a class="size_s3 btn_pink_blueWrite" onclick="lecturePaymentDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[j].itemCd+'\'\,'+data.resultList[i].programItem[0].sold+','+data.resultList[i].programItem[0].eold+','+data.resultList[i].grpcd.recSdate+','+data.resultList[i].grpcd.recEdate+');">수강신청</a></td>';
 																	
 								}
 								
 								//6월 24일 아침 7시에 구민센터 상세보기 가능하도록 해야함
-								/*if(data.resultList[i].comcd == 'JUNGNANG01'){
+								if(data.resultList[i].comcd == 'JUNGNANG01'){
 		        					tableBody += '<td><a class="size_s2 btn_blue2" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-3423-1070");\'>준비중</a></td>';
 		        				}else if(data.resultList[i].comcd == 'JUNGNANG02'){
 		        					tableBody += '<td><a class="size_s2 btn_blue2" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다. \\n자세한 문의는 아래로 연락 바랍니다. \\n연락처 : 02-436-9200");\'>준비중</a></td>';
@@ -399,8 +399,8 @@ var searchLectureList = function(){
 		        					tableBody += '<td><a class="size_s2 btn_blue2" onclick=\'alert("코로나19에 따른 시설물 제한적 운영으로 \\n정규강좌는 수강신청이 불가 합니다.\\n정규강좌 운영시 공지사항에 게재합니다.");\'>준비중</a></td>';
 		        				}else{
 		        					tableBody += '<td ><a class="size_s2 btn_blue2" onclick="searchLectureDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].sportsCd+'\'\, \''+data.resultList[i].msportsCd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[0].itemCd+'\');">상세보기</a></td>';
-		        				}*/
-									tableBody += '<td ><a class="size_s2 btn_blue2" onclick="searchLectureDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].sportsCd+'\'\, \''+data.resultList[i].msportsCd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[j].itemCd+'\');">상세보기</a></td>';
+		        				}
+									//tableBody += '<td ><a class="size_s2 btn_blue2" onclick="searchLectureDetail(\''+data.resultList[i].comcd+'\'\, \''+data.resultList[i].sportsCd+'\'\, \''+data.resultList[i].msportsCd+'\'\, \''+data.resultList[i].classCd+'\'\, \''+data.resultList[i].programItem[j].itemCd+'\');">상세보기</a></td>';
 						
 							}
 									tableBody += '</tr>';
