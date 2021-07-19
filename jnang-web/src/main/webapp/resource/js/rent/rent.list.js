da3c52c147d0b1f98a5a300d21b3fcbe6e553e2c
rpu19 <rpu19@DESKTOP-J6KIS3P>
2021-07-19 17:12:24 +0900

    코로나 대관 접근 불가

--- jnang-web/src/main/webapp/resource/js/rent/rent.list.js
+++ jnang-web/src/main/webapp/resource/js/rent/rent.list.js
@@ -149,7 +149,7 @@ function selectSport(selCT) {
 								if(data[i].COMCD == 'JUNGNANG08' || data[i].COMCD == 'JUNGNANG09' || data[i].COMCD == 'JUNGNANG10' || data[i].COMCD == 'JUNGNANG18'){
 									var saleAmt = data[i].RENT_AMT;
 									//var info_url = (data[i].info_url != "") ? "<a href='"+ data[i].info_url +"' class='size_m2 btn_green1'>안내</a>" : ""; ;
-									var info_url = (data[i].info_url != "") ? "<a href='"+ data[i].info_url +"' class='size_m2 btn_green1' onclick=\"alert('코로나19에 따른 시설물 제한적 운영 및 원활한 \\n온라인 예약접수를 위해 온라인 접수 개시 예정 일자를\\n아래와 같이 안내합니다.. \\n개시 예정일 : 2021년 5월 25일 예정 \\n대관관련 사항은 아래 연락처로 문의바랍니다. \\n연락처: 070-8824-3548');\">안내</a>" : ""; 
+									var info_url = (data[i].info_url != "") ? "<a href='"+ data[i].info_url +"' class='size_m2 btn_green1' onclick=\"alert('코로나19 사회적거리두기 4단계 격상 관련 코로나\\n감염 확산이 지속, 확대됨에 따라\\n실내·야외 체육시설에 대한 운영을 중단 하오니,\\n이용자 여러분의 많은 양해 부탁드립니다.\\n개시 예정일 : 추후 알림 \\n연락처: 070-8824-3548');\">안내</a>" : ""; 
 									var rent_url = (data[i].PLACE_GROUP == "1") ? "write" : "date";
 									dataList += "<tr>"
 									+ "<td>"+ data[i].COMNM +"</td> "
@@ -158,8 +158,8 @@ function selectSport(selCT) {
 									+ "<td>"+ info_url +"</td>"
 									//console.log(data[i].COMNM,data[i].COMCD);
 									//+ "<td><a href='/calendar/?q="+ data[i].PLACE_CD +"' class='size_m2 btn_green1'>대관 현황</a></td>"
-									+ "<td><a href='#' class='size_m2 btn_gray1' onclick=\"alert('코로나19에 따른 시설물 제한적 운영 및 \\n원활한 온라인 예약접수를 위해 온라인 접수 개시 예정 일자를\\n아래와 같이 안내합니다.\\n개시 예정일 : 2021년 5월 25일 예정\\n대관관련 사항은 아래 연락처로 문의바랍니다.\\n연락처: 070-8824-3548');\")>준비중</a></td>"
-									+ "<td><a href='#' class='size_m2 btn_gray1' onclick=\"alert('코로나19에 따른 시설물 제한적 운영 및 \\n원활한 온라인 예약접수를 위해 온라인 접수 개시 예정 일자를\\n아래와 같이 안내합니다.\\n개시 예정일 : 2021년 5월 25일 예정\\n대관관련 사항은 아래 연락처로 문의바랍니다.\\n연락처: 070-8824-3548');\")>준비중</a></td>"
+									+ "<td><a href='#' class='size_m2 btn_gray1' onclick=\"alert('코로나19 사회적거리두기 4단계 격상 관련 코로나\\n감염 확산이 지속, 확대됨에 따라\\n실내·야외 체육시설에 대한 운영을 중단 하오니,\\n이용자 여러분의 많은 양해 부탁드립니다.\\n개시 예정일 : 추후 알림 \\n연락처: 070-8824-3548');\")>준비중</a></td>"
+									+ "<td><a href='#' class='size_m2 btn_gray1' onclick=\"alert('코로나19 사회적거리두기 4단계 격상 관련 코로나\\n감염 확산이 지속, 확대됨에 따라\\n실내·야외 체육시설에 대한 운영을 중단 하오니,\\n이용자 여러분의 많은 양해 부탁드립니다.\\n개시 예정일 : 추후 알림 \\n연락처: 070-8824-3548');\")>준비중</a></td>"
 									//+ "<td><a href='/rent/"+ rent_url +"/?q="+ data[i].PLACE_CD +"/0' class='size_m2 btn_green1' onclick=\"\")>대관 신청</a></td>"
 									+ "</tr>";
 								}else if(data[i].COMCD == 'JUNGNANG07'){
